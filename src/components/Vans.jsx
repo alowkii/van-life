d557b2b9e045b56f480c956028d20540bce2f49c
alowkii <paalok333@gmail.com>
2025-02-05 00:01:16 +0530

    Moved server.js and completed main-component of vans page

--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function Vans() {
+	const [vans, setVans] = useState([])
+	useEffect(() => {
+		fetch("/api/vans")
+			.then(response => response.json())
+			.then(data => setVans(data.vans))
+			.catch(error => console.error("Error fetching vans: "+error))
+	}, [])
+
+	const vanElements = vans.map(van => (
+		<div key={van.id} className="van-title">
+			<img src={van.imageUrl} alt={van.name} />
+			<div className="van-info">
+				<h3>{van.name}</h3>
+				<p>${van.price}<span>/day</span></p>
+			</div>
+			<i className={`van-type ${van.type} selected`}>{van.type}</i>
+		</div>
+	))
+
 	return (
-		<main>
-			<h1>Vans</h1>
-			<p>This is the vans page.</p>
+		<main className="vans-page">
+			<div className="van-list">
+				{vanElements}
+			</div>
 		</main>
 	)
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { makeServer } from './mirage/server';
+
+makeServer();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
